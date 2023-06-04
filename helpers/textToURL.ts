56b63f13cd5b0e3cb77099337df419bd26b3d8ee
podpison <au25557@yandex.ru>
2023-06-04 13:15:52 +0500

    eslint bugs fix

--- helpers/textToURL.ts
+++ helpers/textToURL.ts
@@ -1,5 +1,5 @@
-const textToURL = (text: string) => {
-  return text.toLowerCase().split(' ').map((i, index) => index === 0 ? i : i[0].toUpperCase() + i.slice(1)).join('');
+const textToURL = (text: string = '') => {
+  return text?.toLowerCase().split(' ').map((i, index) => index === 0 ? i : i[0].toUpperCase() + i.slice(1)).join('');
 };
 
 export default textToURL
